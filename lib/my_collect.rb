e2c22b3045a6d2a24ad2fe0c285ae91b4def6d9e
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-10 18:44:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,7 @@
 list = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect = puts array
 my_collect(array) do |name|
   name.split(list).first
 end
